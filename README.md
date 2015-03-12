e058240e6659c1b56a30b540fdf060c5de09c5e7
Jeffrey Wear <jeffreywear@gmail.com>
2015-03-12 14:00:14 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# redis-status
+
+A node module that checks the health of a Redis server, suitable for use by a
+service's "health" route.
+
+## Installation
+
+```js
+npm install redis-status --save
+```
+
+## Usage
+
+```js
+var express = require('express');
+var router = express.Router();
+
+// Construct a `RedisStatus` object configured to check the status of
+// the Redis server named 'foo' at `redis//localhost:6379`.
+var fooStatus = require('redis-status')({
+  name: 'foo',
+  port: 6379,
+  host: 'localhost'
+});
+
+// If 'foo' is healthy, this route will print 'great'; otherwise it will print
+// the reason that 'foo' is not healthy. A monitoring service like Webmon or
+// Pingdom can raise non-'great' responses as alerts.
+router.get('/health', function(req, res) {
+  fooStatus.checkStatus(function(err) {
+    res.send(err || 'great');
+  });
+});
+```
+
+## Contributing
+
+We welcome pull requests! Please lint your code.
+
+## Release History
+
+* 1.0.0 Initial release
---
+++ index.js
@@ -0,0 +1,84 @@
+var redis = require('redis');
+
+/**
+ * Creates a `RedisStatus` object configured to check the status of the specified Redis server.
+ *
+ * @param {object} config
+ *    @property {string} name - An arbitrary name for the server, included in error messages.
+ *    @property {int} port - The port of the Redis server to which to connect.
+ *    @property {string} host - The host of the Redis server to which to connect.
+ *    @property {string=} password - (Optional) The password of the Redis server to which to connect.
+ *      Defaults to none.
+ *    @property {number=} memoryThreshold - (Optional) The maximum amount of memory which the Redis
+ *      server is expected to use when healthy:
+ *        - If you use Redis as an LRU cache, set this to the value of the server's `maxmemory`
+ *          configuration directive.
+ *        - If you use Redis for pub/sub, set this (via observation) to the amount of memory used by
+ * the server 's runtime operations (most likely something like 10MB).
+ *        - Leave this unset if your Redis deployment is autoscaled.
+ *      Defaults to none.
+ */
+function RedisStatus(config) {
+  if (!(this instanceof RedisStatus)) {
+    return new RedisStatus(config);
+  }
+
+  this._name = config.name;
+  this._port = config.port;
+  this._host = config.host;
+  this._password = config.password;
+  this._memoryThreshold = config.memoryThreshold;
+}
+
+/**
+ * Checks the status of the Redis server.
+ *
+ * The server is considered healthy if:
+ *    - it is reachable;
+ *    - and if the server is using less memory than is specified by this object's memory threshold
+ *      (if a threshold was specified when this object was created).
+ *
+ * @param {function<string=>} callback - A function to call with the status: `undefined` if the
+ *    server is healthy, or a string describing the reason that the server is unhealthy.
+ */
+RedisStatus.prototype.checkStatus = function(callback) {
+  var redisClient = redis.createClient(this._port, this._host, {
+    auth_pass: this._password
+  }).on('error', function() {
+    // If Redis is not responsive, `node_redis` will emit an error on the next turn of the event
+    // loop. If we don't provide an error handler, that error will bring down the process. Providing
+    // an error handler will cause `node_redis` to begin attempting to reconnect--but the ping below
+    // will force the matter.
+  });
+
+  // Ensure that our Redis instance is responsive.
+  var self = this;
+  redisClient.ping(function(err, pong) {
+    if (err || (pong !== 'PONG')) {
+      callback(self._name + ' Redis instance is not responsive.');
+      return;
+    }
+
+    if (!self._memoryThreshold) {
+      callback(); // Success.
+    } else {
+      redisClient.info('memory', function(err, info) {
+        if (err) {
+          callback(self._name + ' Redis instance is not responsive.');
+          return;
+        }
+
+        // '# Memory\r\nused_memory:1086352…' -> ['# Memory', 'used_memory:1086352'] ->
+        // 'used_memory:1086352' -> ['used_memory', '1086352'] -> 1086352
+        var usedMemory = parseInt(info.split('\r\n')[1].split(':')[1]);
+        if (usedMemory > self._memoryThreshold) {
+          callback(this._name + ' Redis instance is using abnormally high memory.');
+        } else {
+          callback(); // Success.
+        }
+      });
+    }
+  });
+};
+
+module.exports = RedisStatus;
